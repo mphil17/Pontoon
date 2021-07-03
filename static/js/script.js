b69ba9c5a7e391f78e662af17870a55b61a2abe2
mphil17 <mattphil48@gmail.com>
2021-07-03 16:52:07 +0000

    Minimal viable product complete.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,28 @@
 yourCards = [];
+total = 0;
+
+function initialCardPick(){
+    let card1 = Math.floor((Math.random() * 13) + 1);
+    document.getElementById("cardHolder").innerHTML = card1;
+    let card2 = Math.floor((Math.random() * 13) + 1);
+    document.getElementById("cardHolder2").innerHTML = card2;
+    document.getElementById("start").disabled = true;
+    if (card1 > 10) {
+        card1 = 10;
+    }
+    else {
+        card1 = card1;
+    }
+    if (card2 > 10) {
+        card2 = 10;
+    }
+    else {
+        card2 = card2;
+    }
+    yourCards.push(card1, card2)
+    document.getElementById("currentCards").innerHTML = yourCards;
+    addCards();
+}
 
 function cardPick(){
     let card = Math.floor((Math.random() * 13) + 1);
@@ -29,8 +53,14 @@ function addCards() {
       }
 }
 
+function stick() {
+    document.getElementById("twist").disabled = true;
+    document.getElementById("finalTotal").innerHTML = document.getElementById("currentTotal").innerHTML;
+}
+
 function restart() {
     document.getElementById("twist").disabled = false;
     yourCards.splice(0, yourCards.length);
-    cardPick();
+    document.getElementById("finalTotal").innerHTML = "";
+    initialCardPick();
 }
